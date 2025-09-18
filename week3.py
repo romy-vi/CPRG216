02f0826f6a5c92754deb6d0a0cc829401b761b5e
romy-vi <romy.vicente@edu.sait.ca>
2025-09-18 16:05:35 -0600

    CPRG216

--- week3.py
+++ week3.py
@@ -7,14 +7,26 @@ variables, operators and other stuff
 #assignment 
 
 x = 5 # int
-
 y = 4.5 # float
-
 msg = "Hello" # string
-
 condition = True # Bool
-
 print("The value of the variable x is:", x, "and the type is:", type(x))
 print("The value of the variable y is:",y, "and the type is:",type(y))
 print("The value of the variable msg is:",msg, "and the type is:",type(msg))
 print("The value of the variable condition is:",condition, "and the type is:",type(condition))
+
+# some functions call : print, input, int, float, str, bool
+num_as_text = "43"
+
+num_as_num = int(num_as_text) # cnverting string (text) to num
+
+print(num_as_text, type(num_as_text)) # will print as a text
+print(num_as_num, type (num_as_num)) # will it print?
+print(str(num_as_num)) # equivalent
+num = 3
+num_f = float(3)
+
+num2 = 3.4
+num2_i = int(num2)
+
+num2_as_text = str(num2)
