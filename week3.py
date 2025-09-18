0da7a92c5b642a3fa38703ab5ce1223a7b487353
romy-vi <romy.vicente@edu.sait.ca>
2025-09-18 16:46:31 -0600

    CPRG216

--- week3.py
+++ week3.py
@@ -23,6 +23,7 @@ num_as_num = int(num_as_text) # cnverting string (text) to num
 print(num_as_text, type(num_as_text)) # will print as a text
 print(num_as_num, type (num_as_num)) # will it print?
 print(str(num_as_num)) # equivalent
+
 num = 3
 num_f = float(3)
 
@@ -30,3 +31,21 @@ num2 = 3.4
 num2_i = int(num2)
 
 num2_as_text = str(num2)
+
+
+# Using input function.. Note input function always return to a string (text)
+
+year_of_birth = input("Please enter your year of birth\n")
+print("Your age is ",2025 - int(year_of_birth))
+
+
+# problem 
+
+
+user_input = input("Enter the radius of a circle")
+
+area = 3.14 * user_input ** 2
+
+
+print("The area of the circle is", area, type(area))
+
